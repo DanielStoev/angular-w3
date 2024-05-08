358f4664341f842d3d7ba24adc79245a778e8807
DanielStoev <dannybob63@gmail.com>
2024-05-09 02:38:40 +0300

    Made update and delete button to show only when note is selected

--- notepad/src/app/app.component.ts
+++ notepad/src/app/app.component.ts
@@ -1,22 +1,28 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, FormsModule],
+  imports: [NgFor, FormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'notepad';
   id: number = 0;
+  selectedNote: number = -1;
   editingIndex: number = -1;
   noteTitle: string = '';
   noteContent: string = '';
 
-  notes: { title: string; content: string; noteId: number }[] = [];
+  notes: {
+    title: string;
+    content: string;
+    noteId: number;
+    selected: boolean;
+  }[] = [];
 
   saveNote(title: string, content: string) {
     if (this.editingIndex !== -1) {
@@ -25,15 +31,23 @@ export class AppComponent {
           title,
           content,
           noteId: this.notes[this.editingIndex].noteId,
+          selected: false,
         };
-        this.resetForm();
       }
     } else {
       if (title && content) {
-        this.notes.push({ title, content, noteId: this.id++ });
-        this.resetForm();
+        this.notes.push({ title, content, noteId: this.id++, selected: false });
       }
     }
+    this.resetForm();
+  }
+
+  selectNote(index: number) {
+    if (this.selectedNote !== -1) {
+      this.notes[this.selectedNote].selected = false;
+    }
+    this.selectedNote = index;
+    this.notes[index].selected = true;
   }
 
   editNote(index: number) {
